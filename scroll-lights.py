c1166ae0e4af1307486711ba2db461b104208104
Don Franke <franke.don@gmail.com>
2021-11-02 22:05:03 -0500

    Cleaned up code

---
+++ scroll-lights.py
@@ -0,0 +1,34 @@
+from gpiozero import LED
+from time import sleep
+from gpiozero import Button
+
+button = Button(10)
+led1= LED(17)
+led2= LED(27)
+led3= LED(22)
+led4 = LED(9)
+led5 = LED(11)
+led6 = LED(5)
+leds = [led1,led2,led3,led4,led5,led6]
+
+timer = 0.1
+
+def flash_lights():
+    while(True):
+        i = 0
+        for led in leds:
+            if(i>0):
+                led.on()
+                sleep(timer)
+                led.off()
+            i += 1
+
+        i = 0
+        for led in reversed(leds):
+            if(i>0):
+                led.on()
+                sleep(timer)
+                led.off()
+            i += 1
+
+flash_lights()
